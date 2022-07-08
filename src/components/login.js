5c6482f1c80be18919a5b9750729b06a330a269e
felipeluis223 <lf9072362@gmail.com>
2022-07-07 21:36:19 -0300

    Estilização da página Login

--- src/components/login.js
+++ src/components/login.js
@@ -1,33 +1,35 @@
 import { useState } from 'react'
+import { ThemeProvider } from '@mui/material'
 import '../components/styles/styleLogin.css'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import LightTheme from '../themes/light.js'
 
 function Login(){
 	const [emailUser, setEmailUser] = useState('')
 	const [passwordUser, setPasswordUser] = useState('')
 
 	return (
-		<section className="containerGeral">
-			<div className="containerLogin">
-				<header className="containerCirclePizzaiolo">
-					<div className="">IMG</div>
-				</header>
-
-				<section className="containerLoginDados">
-					<h1>Bem vindo!</h1>
-					<div className="mb-4">
-						<TextField id="outlined-basic" label="E-mail" variant="outlined" />
-					</div>
-					<div className="mb-4">
-						<TextField id="outlined-basic" label="Senha" variant="outlined" />
+		<ThemeProvider theme={LightTheme}>
+			<section className="containerGeral">
+				<div className="containerLogin">
+					<div className="containerCirclePizzaiolo">
 					</div>
 
-					<Button variant="contained">Login</Button>
-				</section>
-			</div>
-		</section>
+					<section className="containerLoginDados">
+						<h1>Bem vindo!</h1>
+						<div className="mb-4">
+							<TextField id="outlined-basic" label="E-mail" variant="outlined" />
+						</div>
+						<div className="mb-4">
+							<TextField id="outlined-basic" label="Senha" variant="outlined" color="primary"/>
+						</div>
 
+						<Button variant="contained">Login</Button>
+					</section>
+				</div>
+			</section>
+		</ThemeProvider>
 	)
 }
 
---
+++ src/themes/light.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material'
+
+const LightTheme = createTheme({
+	palette:{
+		primary: {
+			//#fa4b00;
+			main: "#fa4b00",
+			dark: "#fa4b00",
+			light: "#fa4b00",
+			//contrastText:
+		}
+	}
+});
+
+export default LightTheme;
